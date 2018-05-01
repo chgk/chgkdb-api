adbc1c71f97d4fc9b2e6e5e3292b581be627855b
Roman Semizarov <semizarov@yandex-team.ru>
2018-05-01 19:49:33 +0300

    Initial commit

---
+++ src/Dto/VerifyQuestionsRequest.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Dto;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource(
+ *      collectionOperations={
+ *          "post"={
+ *              "method"="POST",
+ *              "path"="/questions/validate",
+ *
+ *              "swagger_context" = {
+ *                  "responses" = {
+ *                      "200" = {
+ *                          "description" = "The package is ok",
+ *                          "schema" = {
+ *                              "type" = "object",
+ *                              "properties" = {
+ *                                  "html" = {
+ *                                      "type" = "string"
+ *                                  }
+ *                              }
+ *                          }
+ *                      },
+ *                      "400" = {
+ *                          "description" = "There an error during parsing",
+ *                          "schema" = {
+ *                              "type" = "object",
+ *                              "properties" = {
+ *                                  "error" = {
+ *                                      "type" = "string",
+ *                                  },
+ *                                  "line" = {
+ *                                      "type" = "integer"
+ *                                  }
+ *                              }
+ *                          }
+ *                      },
+ *                  },
+ *                  "summary" = "Validates the chgk package",
+ *                  "consumes" = {
+ *                      "application/json"
+ *                  },
+ *                  "produces" = {
+ *                      "application/json"
+ *                  }
+ *              }
+ *          }
+ *      },
+ *      itemOperations={},
+ * )
+ */
+class VerifyQuestionsRequest
+{
+    /**
+     * @var string Text to verify
+     * @Assert\NotBlank
+     * @ApiProperty(
+     *     attributes={
+     *          "swagger_context"={
+     *              "type"="string",
+     *              "example"="Чемпионат:
+Мой турнир
+
+Тур:
+1
+
+Вопрос 1:
+Сколько будет 2x2?
+
+Ответ:
+4
+
+Зачёт:
+4.0
+
+Комментарий:
+А не 5.
+
+Автор:
+Василий Пукин"
+     *
+     *           }
+     *     }
+     * )
+     */
+    public $text;
+}
---
+++ src/EventSubscriber/QuestionsVerifySubscriber.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Dto\VerifyQuestionsRequest;
+use Chgk\ChgkDb\Parser\Iterator\FileLineIterator;
+use Chgk\ChgkDb\Parser\Iterator\TextLineIterator;
+use Chgk\ChgkDb\Parser\ParserFactory\ParserFactory;
+use Chgk\ChgkDb\Parser\TextParser\Exception\ParseException;
+use Chgk\ChgkDb\Parser\TextParser\TextParser;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class QuestionsVerifySubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var ParserFactory
+     */
+    private $parserFactory;
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['validateQuestions', EventPriorities::POST_VALIDATE],
+        ];
+    }
+
+    /**
+     * QuestionsVerifySubscriber constructor.
+     * @param ParserFactory $parserFactory
+     */
+    public function __construct(ParserFactory $parserFactory)
+    {
+        $this->parserFactory = $parserFactory;
+    }
+
+    /**
+     * @param GetResponseForControllerResultEvent $event
+     * @throws \Chgk\ChgkDb\Parser\ParserFactory\UnregisteredParserException
+     */
+    public function validateQuestions(GetResponseForControllerResultEvent $event)
+    {
+        $request = $event->getRequest();
+        if ($request->attributes->get('_route') !== 'api_verify_questions_requests_post_collection')
+        {
+            return;
+        }
+
+        /** @var VerifyQuestionsRequest $verifyRequest */
+        $verifyRequest = $event->getControllerResult();
+
+        $parser = $this->parserFactory->getParser('text');
+        $iterator = new TextLineIterator($verifyRequest->text);
+        try {
+            $package = $parser->parse($iterator);
+            $formatter = \Chgk\ChgkDb\Parser\Formatter\HtmlFormatter::create();
+
+            $html = $formatter->format($package);
+
+
+            $event->setResponse(new JsonResponse(['html' => $html], 200));
+        } catch (ParseException $e) {
+            $event->setResponse(new JsonResponse(['error' => $e->getMessage(), 'line' => $e->getLineNumber()], 400));
+        }
+    }
+}
